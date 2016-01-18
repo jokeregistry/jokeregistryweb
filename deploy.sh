9a9549bdb075f1d26deb306bd92c9d850a3149a5
Chris Sinchok <chris@sinchok.com>
2016-01-17 18:28:42 -0600

    Finalizing the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash -e
 
+if [ `git rev-parse --abbrev-ref HEAD` != "master" ]; then
+	echo "You should only deploy on master!"
+	exit 1
+fi
+
 EXPORT_DIRECTORY=$(mktemp -d -t jrw)
 
 # Export to a local directory
-git archive master | tar -x -C $EXPORT_DIRECTORY
+git archive HEAD | tar -x -C $EXPORT_DIRECTORY
 
 # Push up the code
 rsync -a $EXPORT_DIRECTORY/ www-data@jokeregistry.online:/www/jokeregistryweb/
 
-ssh www-data@jokeregistry.online << EOF
+ssh -q www-data@jokeregistry.online << EOF
   cd /www/jokeregistryweb/
   export DJANGO_SETTINGS_MODULE=jokeregistryweb.production
   source bin/activate
@@ -19,14 +24,15 @@ ssh www-data@jokeregistry.online << EOF
   # Collect the static files
   python manage.py collectstatic --noinput
 
-  
+  # Run DB migrations
+  python manage.py migrate --noinput
+
+  # Reload the web server
+  touch /etc/uwsgi-emperor/vassals/jokeregistryweb.ini
 EOF
 
-# Collectstatic
-
-# Migrate
-
-# Reload
+# We need to re-run this every 90 days to get new certs...
+# ./letsencrypt-auto certonly --webroot -w /www/jokeregistryweb/public/ -d www.jokeregistry.online -d jokeregistry.online
 
 # Clean up after yourself...
 rm -rf $EXPORT_DIRECTORY
